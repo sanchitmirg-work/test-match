2e723d9b9dcd76702023f7e05f1800ef6ca3e9fb
Sanchit Mirg <sanchitm@Meditab.Local>
2019-01-04 00:07:43 +0530

    bidding upper section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { CalendarModule, PanelModule, OrderListModule } from 'primeng/primeng';
+import { CalendarModule, PanelModule, OrderListModule, CardModule, DropdownModule } from 'primeng/primeng';
 import { DataViewModule } from 'primeng/dataview';
 import { MainComponent } from './main/main.component';
 import { AlertModule } from 'ngx-bootstrap';
@@ -29,6 +29,8 @@ import { BiddingComponent } from './components/bidding/bidding.component';
     DataViewModule,
     PanelModule,
     OrderListModule,
+    CardModule,
+    DropdownModule,
     AlertModule.forRoot()
   ],
   providers: [
--- src/app/components/bidding/bidding.component.ts
+++ src/app/components/bidding/bidding.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PlayersService } from '../../services/players.service';
 import { OEPLMember } from '../../modals/OEPLMember';
 import { ViewEncapsulation } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-bidding',
@@ -12,6 +13,9 @@ import { ViewEncapsulation } from '@angular/core';
 export class BiddingComponent implements OnInit {
 
   players: OEPLMember[] = [];
+  teams: SelectItem[];
+  selectedTeam: any;
+  selectedPlayer: OEPLMember = new OEPLMember();
   constructor(
     private service: PlayersService
   ) { }
@@ -23,6 +27,17 @@ export class BiddingComponent implements OnInit {
           this.players = response;
         }
       });
+
+    this.teams = [
+      { label: 'Royals', value: 'Royals' },
+      { label: 'Riders', value: 'Riders' },
+      { label: 'Rangers', value: 'Rangers' },
+      { label: 'Rovers', value: 'Rovers' }
+    ];
+  }
+
+  onPlayerSelection(player) {
+    this.selectedPlayer = player;
   }
 
 }
